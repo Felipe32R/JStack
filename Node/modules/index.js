8a4578a0aa1cedc52e5343846ec931b8f39ac7da
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-05-26 20:14:34 -0300

    module.exports

---
+++ Node/modules/index.js
@@ -0,0 +1,9 @@
+const {printName, lastName} = require('./printName')
+printName(`Felipe ${lastName}` )
+
+
+
+//módulos nativos
+
+const os = require('os');
+console.log(os.type());
---
+++ Node/modules/printName.js
@@ -0,0 +1,12 @@
+/* function printName(name) {
+  console.log(name);
+} */
+
+exports.printName = (name) =>{
+  console.log(name);
+}
+
+//const lastName = 'Ramalho'
+exports.lastName = 'Ramalho'
+
+//module.exports = {printName, lastName}
