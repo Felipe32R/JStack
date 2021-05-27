544561cb72a00aa8b0a89a9053621957d9f13918
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-05-27 10:31:19 -0300

    index

---
+++ Node/firstapi/src/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end('<h1>Hello world!</h1>');
+});
+
+server.listen(3000, () =>
+  console.log('🔥 Server started at http://localhost:3000')
+);
