7111137e2229682e5b308b4df88b5d36ec26c6e0
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-05-30 20:15:54 -0300

    routes

---
+++ Node/firstapi/src/routes.js
@@ -0,0 +1,20 @@
+const UserController = require('./controllers/UserController');
+
+module.exports = [
+  {
+    endpoint: '/users',
+    method: 'GET',
+    handler: UserController.listUsers,
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'GET',
+    handler: UserController.getUserById,
+  },
+  {
+    endpoint: '/users',
+    method: 'POST',
+    handler: UserController.createUser,
+  },
+ 
+];
