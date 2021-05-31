3ec3124ea78e720cb4be0fd555eea53887403aed
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-05-31 18:40:20 -0300

    controllers

---
+++ Node/firstapi/src/controllers/UserController.js
@@ -0,0 +1,48 @@
+const users = require('../mocks/users')
+const { request } = require('https');
+
+
+module.exports = {
+
+  listUsers(request, response){
+
+    const { order } = request.query;
+
+    const sortedUsers = users.sort((a, b) => {
+      if(order === 'desc'){
+        return a.id < b.id ? 1 : -1;
+      }
+        return a.id > b.id ? 1 : -1;
+    })
+
+    response.send(200, sortedUsers)
+  },
+  getUserById(request, response) {
+
+    const {id} = request.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+
+    if(!user) {
+      return response.send(400, {error: 'User not found.'})
+    }
+      response.send(200, user)
+    
+  },
+
+  createUser(request,response){
+    const { body } = request;
+    const lastUserId = users[users.length -1].id;
+
+    const newUser = {
+      id: lastUserId +1,
+      name: body.name
+    }
+
+    users.push(newUser)
+
+    response.send(200, newUser)
+
+  },
+}
