45e29186a64159f55984bdf14b1cd097f3f9dfba
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-06-02 21:03:57 -0300

    routes

--- Node/firstapi/src/routes.js
+++ Node/firstapi/src/routes.js
@@ -16,5 +16,15 @@ module.exports = [
     method: 'POST',
     handler: UserController.createUser,
   },
+  {
+    endpoint: '/users/:id',
+    method: 'PUT',
+    handler: UserController.updateUser,
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'DELETE',
+    handler: UserController.deleteUser,
+  },
  
 ];
