7204da2222312f1405733a375bdfb11d59cebf28
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-06-08 23:51:59 -0300

    ContactController

---
+++ Node/mycontacts/src/app/controllers/ContactController.js
@@ -0,0 +1,24 @@
+class ContactController {
+  index() {
+    // listar todos os registros
+  }
+
+  show() {
+    // Obter UM registro
+  }
+
+  store() {
+    // criar novo registro
+  }
+
+  update() {
+    // editar um registro
+  }
+
+  delete() {
+    // deletar um registro
+  }
+}
+
+module.exports = new ContactController();
+// exporta apenas uma classe salva em cache, não gera uma nova
