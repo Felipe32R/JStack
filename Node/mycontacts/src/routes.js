d976f2fa0068c95137309b70b25cf0f85fd8db12
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-06-12 21:38:24 -0300

    routes.js

--- Node/mycontacts/src/routes.js
+++ Node/mycontacts/src/routes.js
@@ -4,10 +4,14 @@ const router = express.Router(); */
 
 const { Router } = require('express');
 
+const ContactController = require('./app/controllers/ContactController');
+
 const router = Router();
 
-router.get('/', (request, response) => {
-  response.send('Hello World');
-});
+router.get('/contacts', ContactController.index);
+router.get('/contacts/:id', ContactController.show);
+router.delete('/contacts/:id', ContactController.delete);
+router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
 
 module.exports = router;
