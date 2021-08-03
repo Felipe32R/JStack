be1d40a026f39eb32be0c3acb72369e34aae68f6
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-08-03 19:00:41 -0300

    backend

---
+++ mycontacts/backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
---
+++ mycontacts/backend/src/routes.js
@@ -0,0 +1,24 @@
+/* const express = require('express');
+
+const router = express.Router(); */
+
+const { Router } = require('express');
+
+const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
+
+const router = Router();
+
+router.get('/contacts', ContactController.index);
+router.get('/contacts/:id', ContactController.show);
+router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
+router.delete('/contacts/:id', ContactController.delete);
+
+router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
+router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
+
+module.exports = router;
