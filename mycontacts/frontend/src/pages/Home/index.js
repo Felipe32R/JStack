9e300b02b047e758d7138e7274f3f740def307d8
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-10-27 14:04:11 -0300

    linking backend

--- mycontacts/frontend/src/pages/Home/index.js
+++ mycontacts/frontend/src/pages/Home/index.js
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import {
-  Container, Header, ListContainer, Card,
+  Container,
+  Header,
+  ListHeader,
+  Card,
   InputSearchContainer,
 } from './styles';
 
@@ -11,59 +15,79 @@ import trash from '../../assets/images/trash.svg';
 // import Loader from '../../components/Loader';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  }, [orderBy]);
+
+  function handleToggleOrderBy() {
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  }
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <Container>
-
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquise pelo nome" />
+        <input
+          value={searchTerm}
+          type="text"
+          placeholder="Pesquise pelo nome"
+          onChange={handleChangeSearchTerm}
+        />
       </InputSearchContainer>
 
       <Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' contatos' : ' contato'}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button">
-            <span>Nome</span>
-            <img src={arrow} alt="Seta" />
-          </button>
-        </header>
-      </ListContainer>
+      <ListHeader orderBy={orderBy}>
+        <button type="button" onClick={handleToggleOrderBy}>
+          <span>Nome</span>
+          <img src={arrow} alt="Seta" />
+        </button>
+      </ListHeader>
 
-      <Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong>Felipe Ramalho</strong>
-            <small>instagram</small>
+      {contacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              {contact.category_name && <small>{contact.category_name}</small>}
+            </div>
+
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
           </div>
 
-          <span>felipe@email</span>
-          <span>(31) 9999-9999</span>
-        </div>
-
-        <div className="actions">
-          <Link to="/edit/123"><img src={edit} alt="Editar" /></Link>
-          <button type="button">
-            <img src={trash} alt="Excluir" />
-          </button>
-        </div>
-      </Card>
-
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src={edit} alt="Editar" />
+            </Link>
+            <button type="button">
+              <img src={trash} alt="Excluir" />
+            </button>
+          </div>
+        </Card>
+      ))}
     </Container>
   );
 }
-
-fetch('http://localhost:3001/contacts')
-  .then(async (response) => {
-    const json = await response.json();
-    console.log('json', json);
-
-    json.forEach((contact) => {
-      console.log(contact.name);
-    });
-  })
-  .catch((error) => {
-    console.log('error', error);
-  });
--- mycontacts/frontend/src/pages/Home/styles.js
+++ mycontacts/frontend/src/pages/Home/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin-top: 32px;
   position: relative;
+  margin-bottom: 100px;
 `;
 
 export const InputSearchContainer = styled.div`
@@ -50,24 +51,26 @@ export const Header = styled.header`
   }
 `;
 
-export const ListContainer = styled.div`
+export const ListHeader = styled.header`
   margin-top: 24px;
+  margin-bottom: 8px;
 
-  header{
-
-    margin-bottom: 8px;
     button{
       background: transparent;
       border: none;
       display: flex;
       align-items: center;
-    }
     span{
       color: ${({ theme }) => theme.colors.primary.main};
       margin-right: 8px;
       font-weight: bold;
     }
+    img{
+      transition: .3s ease;
+      transform: ${({ orderBy }) => (orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)')};
+    }
   }
+
 `;
 
 export const Card = styled.div`
